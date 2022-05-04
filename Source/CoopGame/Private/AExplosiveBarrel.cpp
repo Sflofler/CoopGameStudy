caccfb249ed86c15eabab2e033e3e81fea9b49e1
Tiago Stockler <tiagodstockler@gmail.com>
2022-05-04 19:46:45 -0300

    Add explosive barrels

---
+++ Source/CoopGame/Private/AExplosiveBarrel.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AExplosiveBarrel.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AAExplosiveBarrel::AAExplosiveBarrel()
+{
+	HealthComponent = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComponent"));
+	HealthComponent->OnHealthChanged.AddDynamic(this, &AAExplosiveBarrel::HandleHealthChanged);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	StaticMeshComponent->SetSimulatePhysics(true);
+	StaticMeshComponent->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = StaticMeshComponent;
+
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComponent"));
+	RadialForceComponent->SetupAttachment(StaticMeshComponent);
+	RadialForceComponent->Radius = 250;
+	RadialForceComponent->bImpulseVelChange = true;
+	RadialForceComponent->bAutoActivate = false;
+	RadialForceComponent->bIgnoreOwningActor = true;
+
+	ExplosionImpulse = 400.0f;
+}
+
+// Called when the game starts or when spawned
+void AAExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAExplosiveBarrel::HandleHealthChanged(USHealthComponent* OwningHealthComponent, float Health, float HealthDelta,
+	const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if(bExploded)
+	{
+		return;
+	}
+
+	if(Health <= 0.0f)
+	{
+		bExploded = true;
+
+		const FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		StaticMeshComponent->AddImpulse(BoostIntensity, NAME_None, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+
+		StaticMeshComponent->SetMaterial(0, ExplodedMaterial);
+
+		RadialForceComponent->FireImpulse();
+
+		const TArray<AActor*> IgnoredActors;
+
+		UGameplayStatics::ApplyRadialDamage(this, 50.0f, GetActorLocation(), 250.0f, DamageTypeClass, IgnoredActors);
+	}
+}
+
---
+++ Source/CoopGame/Public/AExplosiveBarrel.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SHealthComponent.h"
+#include "GameFramework/Actor.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "AExplosiveBarrel.generated.h"
+
+UCLASS()
+class COOPGAME_API AAExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USHealthComponent* HealthComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	URadialForceComponent* RadialForceComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TSubclassOf<UDamageType> DamageTypeClass;
+
+	bool bExploded;
+
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	float ExplosionImpulse;
+
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	UParticleSystem* ExplosionEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	UMaterialInterface* ExplodedMaterial;
+
+	UFUNCTION()
+	void HandleHealthChanged(USHealthComponent* OwningHealthComponent, float Health, float HealthDelta, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+};
